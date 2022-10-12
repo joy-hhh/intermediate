081678daae492acde6a3cdd7fd713d386b1a20f7
“joy-hhh” <joyhhh@outlook.kr>
2022-10-12 09:51:25 +0900

    26

--- 25_isinstance_object.py
+++ 25_isinstance_object.py
@@ -10,4 +10,16 @@ print(isinstance([1,2], list))
 # 파이썬의 모든 클래스는 object 클래스를 직접 혹은 간접 상속한다.
 print(isinstance(Simple, object))
 
+
+class A:
+    pass
+
+class Z(A):
+    pass
+
+issubclass(Z,A)
+
+issubclass(type, object)
+
 print(dir(object))
+
--- 26_special_method.py
+++ 26_special_method.py
@@ -1,6 +1,14 @@
-# 스페셜메소드 : 호출 시점이 약속된 메소드
+# 스페셜 메소드 : 호출 시점이 약속된 메소드
+# __name__
+
+from matplotlib.pyplot import cla
+from regex import D
+from sympy import Wild
+
+
 t = (1,2,3)
-print(len(t))
+print(len(t))   # t.__len__()
+
 itr = iter(t)
 for i in itr:
     print(i, end = ' ')
@@ -15,3 +23,88 @@ for i in itr:
 
 s = t.__str__()
 print(s)
+
+
+# 클래스에 스페셜 메소드 정의하기
+
+class Car:
+    def __init__(self, id):
+        self.id = id
+    def __len__(self):
+        return len(self.id)
+    def __str__(self):
+        return 'Vehicle number : ' + self.id
+    
+def main():
+    c = Car("32러5234")
+    print(len(c))
+    print(str(c))
+    
+main()
+
+
+# iterable 객체가 되게끔 하기
+class Car:
+    def __init__(self, id):
+        self.id = id
+    def __iter__(self):
+        return iter(self.id)
+    
+def main2():
+    c = Car("32러5234")
+    for i in c:
+        print(i, end = ' ')
+        
+main2()
+
+# iterator 객체가 되게끔 하기
+class Coll:    ## 저장소 역할을 하는 클래스를 표현한 결과
+    def __init__(self, d):
+        self.ds = d
+        self.cc = 0    # __next__ 메소드 호출 횟수
+    def __next__(self):
+        if len(self.ds) <= self.cc:
+            raise StopIteration
+        self.cc += 1
+        return self.ds[self.cc -1]
+    
+def main3():
+    co = Coll([1,2,3,4,5])
+    while True:
+        try:
+            i = next(co)
+            print(i, end = ' ')
+        except StopIteration:
+            break
+        
+main3()
+   
+# iterator 객체이자 iterable 객체가 되게끔 하기
+class Coll2:
+    def __init__(self, d):
+        self.ds = d
+    def __next__(self):
+        if len(self.ds) <= self.cc:
+            raise StopIteration
+        self.cc += 1
+        return self.ds[self.cc - 1]
+    def __iter__(self):
+        self.cc = 0
+        print('iter 호출됨')
+        return self   # 이 객체를 그대로 반환함
+    
+def main4():
+    co = Coll2([1,2,3,4,5])
+    for i in co:
+        print(i, end = ' ')
+    for i in co:
+        print(i, end = ' ')
+
+main4()
+
+
+
+
+co = Coll2('hello')
+itr = iter(co)
+itr is co
